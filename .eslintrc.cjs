33d7c30e49203d73aa3e9eb5591315fbcbdfc157
cazac <cazacuvaleriu2@gmail.com>
2023-06-10 13:00:00 +0300

    Configured, husky, eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": "warn",
-  },
-  quotes: ["error", "double"],
-  "no-unused-vars": [
-    "error",
-    { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-  ],
-  "no-console": "error",
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		quotes: ['error', 'double'],
+		'no-unused-vars': [
+			'error',
+			{ varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+		],
+		'no-console': 'error',
+	},
 };
