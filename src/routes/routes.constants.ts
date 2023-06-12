078a13f71fa2c3d7f18976457bce30c2c81a1cb7
cazac <cazacuvaleriu2@gmail.com>
2023-06-12 22:16:35 +0300

    Made routing and navigation component

---
+++ src/routes/routes.constants.ts
@@ -0,0 +1,15 @@
+interface Routes {
+	HOME: string;
+	CONTACTS: string;
+	SIGN_IN: string;
+	SHOP: string;
+	SHOPPING_CART: string;
+}
+
+export const ROUTES: Routes = {
+	HOME: "",
+	CONTACTS: "/contacts",
+	SIGN_IN: "sign-in",
+	SHOP: "/shop",
+	SHOPPING_CART: "/shopping-cart",
+};
