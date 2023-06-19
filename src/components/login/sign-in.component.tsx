aa5d0ee9022dbb4e2e108eb2765c090e63a99808
cazac <cazacuvaleriu2@gmail.com>
2023-06-19 13:58:15 +0300

    Finished sign-in module without api

--- src/components/login/sign-in.component.tsx
+++ src/components/login/sign-in.component.tsx
@@ -4,9 +4,11 @@ import SignInFormComponent from "../sign-in-form/sign-in-form.component.tsx";
 
 const SignInComponent = () => {
 	return (
-		<div className="sign-in-container">
-			<h3>I already have an account</h3>
-			<p>Sign in with your email and password</p>
+		<div className="sign-in">
+			<h3 className="sign-in__title">I already have an account</h3>
+			<p className="sign-in__description">
+				Sign in with your email and password
+			</p>
 			<SignInFormComponent />
 		</div>
 	);
--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 import InputComponent from "../input/input.component.tsx";
 
+import "./sign-in-form.styles.scss";
+import ButtonComponent from "../button/button.component.tsx";
+
 const SignInFormComponent = () => {
 	const [signInValues, setSignInValues] = useState({
 		email: "",
@@ -15,7 +18,7 @@ const SignInFormComponent = () => {
 		setSignInValues({ ...signInValues, [property]: e.target.value });
 	};
 	return (
-		<>
+		<form className="sign-in__form">
 			<InputComponent
 				type="text"
 				placeholder="Email"
@@ -30,7 +33,11 @@ const SignInFormComponent = () => {
 				inputValue={signInValues.password}
 				name="password"
 			/>
-		</>
+			<div className="sign-in__form_buttons">
+				<ButtonComponent content="Sign In" color="black" />
+				<ButtonComponent content="Sign in with google" color="#6f32a8" />
+			</div>
+		</form>
 	);
 };
 
