c4a1580d9c4b596a76c97f07c5ff0886ff1f0324
cazac <cazacuvaleriu2@gmail.com>
2023-06-24 20:15:19 +0300

    Started to work on adapting the sign-in form with useSignInFormik

--- src/routes/authentification/sign-in-form/sign-in-form.component.tsx
+++ src/routes/authentification/sign-in-form/sign-in-form.component.tsx
@@ -1,53 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
+
 import {
 	signInWithGooglePopup,
 	createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils.js";
-
+import { useFormikSignInHook } from "../../../hooks/formikHooks/useSignInFormik.ts";
 import InputComponent from "../../../components/input/input.component.tsx";
-
-import "./sign-in-form.styles.scss";
 import ButtonComponent from "../../../components/button/button.component.tsx";
 
+import "./sign-in-form.styles.scss";
+
 const SignInFormComponent = () => {
-	const [signInValues, setSignInValues] = useState({
-		email: "",
-		password: "",
-	});
+	const { handleChange, values, errors } = useFormikSignInHook();
 
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		await createUserDocumentFromAuth(user);
 	};
 
-	const onChange = (
-		e: React.ChangeEvent<HTMLInputElement>,
-		property: string
-	) => {
-		setSignInValues({ ...signInValues, [property]: e.target.value });
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 	};
+
 	return (
-		<form className="sign-in__form">
+		<form onSubmit={handleSubmit} className="sign-in__form">
 			<InputComponent
 				type="text"
 				placeholder="Email"
-				onChange={onChange}
-				inputValue={signInValues.email}
+				onChangeHandler={handleChange}
+				inputValue={values.email}
 				name="email"
+				error={errors.email}
 			/>
 			<InputComponent
 				type="password"
 				placeholder="Password"
-				onChange={onChange}
-				inputValue={signInValues.password}
+				onChangeHandler={handleChange}
+				inputValue={values.password}
 				name="password"
+				error={errors.password}
 			/>
 			<div className="sign-in__form_buttons">
-				<ButtonComponent content="Sign In" color="black" />
+				<ButtonComponent content="Sign In" color="black" type="submit" />
 				<ButtonComponent
 					onClickHandler={logGoogleUser}
 					content="Sign in with google"
 					color="#6f32a8"
+					type="button"
 				/>
 			</div>
 		</form>
