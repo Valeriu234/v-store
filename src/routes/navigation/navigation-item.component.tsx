dd9beb08a93e34190d0ae79f6af81cae840b7189
cazac <cazacuvaleriu2@gmail.com>
2023-06-24 20:10:08 +0300

    Changed the type of children to a more actual type in navigation-item

--- src/routes/navigation/navigation-item.component.tsx
+++ src/routes/navigation/navigation-item.component.tsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 import { Link } from "react-router-dom";
 
 interface NavigationItemComponentProps {
-	children: JSX.Element;
+	children: React.ReactNode;
 	route: string;
 }
 const NavigationItemComponent = ({
