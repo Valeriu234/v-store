e89e8e6a9c2df50e256d48680d688adf4f79a61e
cazac <cazacuvaleriu2@gmail.com>
2023-06-24 20:08:00 +0300

    Wrapped error boundaries component around the whole app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.scss";
+import { ErrorBoundaryComponent } from "./components/error-boundary/error-boundary.component.jsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<ErrorBoundaryComponent>
+				<App />
+			</ErrorBoundaryComponent>
 		</BrowserRouter>
 	</React.StrictMode>
 );
