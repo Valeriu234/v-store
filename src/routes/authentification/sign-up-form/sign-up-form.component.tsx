55a2cf79ccf49952bb542f438523eba5175fb044
cazac <cazacuvaleriu2@gmail.com>
2023-06-24 20:13:49 +0300

    Adapted sign-up form with the formik useSignUpFormik.ts

--- src/routes/authentification/sign-up-form/sign-up-form.component.tsx
+++ src/routes/authentification/sign-up-form/sign-up-form.component.tsx
@@ -1,58 +1,82 @@
-import React, { useState } from "react";
+import React from "react";
 
-import "./sign-up-form.styles.scss";
+import { useFormikSignUpHook } from "../../../hooks/formikHooks/useSignUpFormik.ts";
 
 import InputComponent from "../../../components/input/input.component.tsx";
 import ButtonComponent from "../../../components/button/button.component.tsx";
 
-const SignUpFormComponent = () => {
-	const [signUpValues, setSignUpValues] = useState({
-		displayName: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	});
+import "./sign-up-form.styles.scss";
 
-	const onChange = (
-		e: React.ChangeEvent<HTMLInputElement>,
-		property: string
-	) => {
-		setSignUpValues({ ...signUpValues, [property]: e.target.value });
+const SignUpFormComponent = () => {
+	const { values, errors, handleChange, handleSubmit } = useFormikSignUpHook();
+
+	const inputData = [
+		{
+			type: "text",
+			inputValue: values.displayName,
+			name: "displayName",
+			placeholder: "Display Name",
+			onChangeHandler: handleChange,
+			error: errors.displayName,
+		},
+		{
+			type: "email",
+			inputValue: values.email,
+			name: "email",
+			placeholder: "Email",
+			onChangeHandler: handleChange,
+			error: errors.email,
+		},
+		{
+			type: "password",
+			inputValue: values.password,
+			name: "password",
+			placeholder: "Password",
+			onChangeHandler: handleChange,
+			error: errors.password,
+		},
+		{
+			type: "password",
+			inputValue: values.password,
+			name: "confirmPassword",
+			placeholder: "Confirm Password",
+			onChangeHandler: handleChange,
+			error: errors.confirmPassword,
+		},
+	];
+	const hasFormikValidationErrors = () => {
+		return Object.entries(errors);
+	};
+
+	const handleSignUpSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (!hasFormikValidationErrors().length) {
+			handleSubmit(event);
+		}
 	};
 	return (
-		<form className="sign-up__form">
-			<InputComponent
-				type="text"
-				placeholder="Display Name"
-				name="displayName"
-				inputValue={signUpValues.displayName}
-				onChange={onChange}
-			/>
-			<InputComponent
-				type="email"
-				inputValue={signUpValues.email}
-				name="email"
-				placeholder="Email"
-				onChange={onChange}
-			/>
-			<InputComponent
-				type="password"
-				inputValue={signUpValues.password}
-				name="password"
-				placeholder="Password"
-				onChange={onChange}
-			/>
-			<InputComponent
-				type="password"
-				inputValue={signUpValues.confirmPassword}
-				name="confirmPassword"
-				placeholder="Confirm password"
-				onChange={onChange}
-			/>
+		<form onSubmit={handleSignUpSubmit} className="sign-up__form">
+			{inputData.map(
+				(
+					{ inputValue, onChangeHandler, placeholder, error, type, name },
+					index
+				) => (
+					<InputComponent
+						type={type}
+						inputValue={inputValue}
+						name={name}
+						placeholder={placeholder}
+						onChangeHandler={onChangeHandler}
+						error={error}
+						key={index}
+					/>
+				)
+			)}
 			<ButtonComponent
 				className="sign-up__form_button"
 				content="Sign Up"
 				color="black"
+				type="submit"
 			/>
 		</form>
 	);
