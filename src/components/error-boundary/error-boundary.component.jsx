b8059bf0c8050ce1255e2303da83d901cf8260d8
cazac <cazacuvaleriu2@gmail.com>
2023-06-24 19:58:25 +0300

    Created error-boundary component

---
+++ src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export class ErrorBoundaryComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { hasError: false, error: undefined };
+	}
+
+	static getDerivedStateFromError(error) {
+		// Update state to indicate an error has occurred
+		return { hasError: true, error: error };
+	}
+
+	componentDidCatch(error, info) {
+		// You can log the error or send it to an error tracking service
+		console.error(error, info);
+	}
+
+	render() {
+		if (this.state.hasError) {
+			// Render fallback UI when an error occurs
+			return <h1>{this.state.error}.</h1>;
+		}
+
+		// Render children components as normal
+		return this.props.children;
+	}
+}
