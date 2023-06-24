742b57abd69f3fdf5e8909173557720c00ece3b4
cazac <cazacuvaleriu2@gmail.com>
2023-06-24 20:07:15 +0300

    Changed the order of the imports

--- src/routes/authentification/login-page/login-page.component.tsx
+++ src/routes/authentification/login-page/login-page.component.tsx
@@ -1,7 +1,7 @@
-import "./login-page.styles.scss";
-
 import SignInComponent from "../login/sign-in.component.tsx";
 import SignUpComponent from "../register/sign-up.component.tsx";
+
+import "./login-page.styles.scss";
 const LoginPageComponent = () => {
 	return (
 		<div className="login-page-container">
