b5245fcb170a1baaafa741f499832c4f78f1edf1
Valeriu <cazacuvaleriu2@gmail.com>
2023-06-27 14:38:42 +0300

    Changed the text of the title and description in the sign-up

--- src/routes/authentification/register/sign-up.component.tsx
+++ src/routes/authentification/register/sign-up.component.tsx
@@ -5,9 +5,9 @@ import "./sign-up.styles.scss";
 const SignUpComponent = () => {
 	return (
 		<div className="sign-up">
-			<h3 className="sign-up__title">I already have an account</h3>
+			<h3 className="sign-up__title">Don't have an account ?</h3>
 			<p className="sign-up__description">
-				Sign in with your email and password
+				Sign up with your email and password
 			</p>
 			<SignUpFormComponent />
 		</div>
