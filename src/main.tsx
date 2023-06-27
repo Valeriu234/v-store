fbabf9a439773c36d27f1002857bc4085b2fbfc0
Valeriu <cazacuvaleriu2@gmail.com>
2023-06-27 14:34:59 +0300

    Wrapped the whole app in user provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.tsx";
-import "./index.scss";
+import { UserProvider } from "./contexts/user.context.tsx";
 import { ErrorBoundaryComponent } from "./components/error-boundary/error-boundary.component.jsx";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<ErrorBoundaryComponent>
-				<App />
+				<UserProvider>
+					<App />
+				</UserProvider>
 			</ErrorBoundaryComponent>
 		</BrowserRouter>
 	</React.StrictMode>
