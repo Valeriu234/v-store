1f2959a6636be3e7f847141dc434ef8389e2992d
Valeriu <cazacuvaleriu2@gmail.com>
2023-06-27 14:37:39 +0300

    Refactored sign in form to a more reusable and maintainable way

--- src/routes/authentification/sign-in-form/sign-in-form.component.tsx
+++ src/routes/authentification/sign-in-form/sign-in-form.component.tsx
@@ -5,49 +5,99 @@ import {
 	createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils.js";
 import { useFormikSignInHook } from "../../../hooks/formikHooks/useSignInFormik.ts";
+
 import InputComponent from "../../../components/input/input.component.tsx";
 import ButtonComponent from "../../../components/button/button.component.tsx";
 
 import "./sign-in-form.styles.scss";
 
 const SignInFormComponent = () => {
-	const { handleChange, values, errors } = useFormikSignInHook();
+	const {
+		handleChange,
+		values,
+		errors,
+		touched,
+		setFieldTouched,
+		setTouched,
+		handleSubmit,
+	} = useFormikSignInHook();
+
+	const signInInputData = [
+		{
+			type: "email",
+			inputValue: values.email,
+			name: "email",
+			placeholder: "Email",
+			onChange: handleChange,
+			error: errors.email,
+			touched: touched.email,
+			setTouched: setFieldTouched,
+		},
+		{
+			type: "password",
+			inputValue: values.password,
+			name: "password",
+			placeholder: "Password",
+			onChange: handleChange,
+			error: errors.password,
+			touched: touched.password,
+			setTouched: setFieldTouched,
+		},
+	];
+
+	const setAllFormikSignInFieldsTouched = () => {
+		setTouched({
+			email: true,
+			password: true,
+		});
+	};
 
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		await createUserDocumentFromAuth(user);
 	};
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	const handleSignInSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		setAllFormikSignInFieldsTouched();
+		handleSubmit();
 	};
 
 	return (
-		<form onSubmit={handleSubmit} className="sign-in__form">
-			<InputComponent
-				type="text"
-				placeholder="Email"
-				onChangeHandler={handleChange}
-				inputValue={values.email}
-				name="email"
-				error={errors.email}
-			/>
-			<InputComponent
-				type="password"
-				placeholder="Password"
-				onChangeHandler={handleChange}
-				inputValue={values.password}
-				name="password"
-				error={errors.password}
-			/>
+		<form onSubmit={handleSignInSubmit} className="sign-in__form">
+			{signInInputData.map(
+				({
+					inputValue,
+					onChange,
+					placeholder,
+					setTouched,
+					touched,
+					name,
+					error,
+					type,
+				}) => (
+					<InputComponent
+						type={type}
+						placeholder={placeholder}
+						onChange={onChange}
+						value={inputValue}
+						name={name}
+						error={error}
+						touched={touched}
+						setTouched={setTouched}
+					/>
+				)
+			)}
+
 			<div className="sign-in__form_buttons">
-				<ButtonComponent content="Sign In" color="black" type="submit" />
+				<ButtonComponent type="submit">Sign IN</ButtonComponent>
 				<ButtonComponent
-					onClickHandler={logGoogleUser}
-					content="Sign in with google"
-					color="#6f32a8"
+					onClick={logGoogleUser}
 					type="button"
-				/>
+					buttonType="google"
+				>
+					Sign in with google
+				</ButtonComponent>
 			</div>
 		</form>
 	);
