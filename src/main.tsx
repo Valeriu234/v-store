8cb545929021254ac8325c79a88ed555580a69b5
Valeriu <cazacuvaleriu2@gmail.com>
2023-07-22 10:12:45 +0300

    Changed the ProductProvider into categoriesProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,23 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { UserProvider } from "./contexts/user.context.tsx";
-import { ProductsProvider } from "./contexts/products.context.tsx";
-import { ErrorBoundaryComponent } from "./components/error-boundary/error-boundary.component.jsx";
-import "./index.scss";
+import { CategoriesProvider } from "./contexts/products.context.tsx";
 import { CartProvider } from "./contexts/cart.context.tsx";
+import { ErrorBoundaryComponent } from "./components/error-boundary/error-boundary.component.jsx";
+
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<ErrorBoundaryComponent>
-				<ProductsProvider>
+				<CategoriesProvider>
 					<UserProvider>
 						<CartProvider>
 							<App />
 						</CartProvider>
 					</UserProvider>
-				</ProductsProvider>
+				</CategoriesProvider>
 			</ErrorBoundaryComponent>
 		</BrowserRouter>
 	</React.StrictMode>
