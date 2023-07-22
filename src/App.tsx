1a58b7ee96fcc79dbec7ef3e3f8bddfc8baa78d5
Valeriu <cazacuvaleriu2@gmail.com>
2023-07-22 09:47:08 +0300

    Introduced Contacts page and cart page in app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
+import { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import HomeComponent from "./routes/home/home.component.tsx";
+import HomeComponent from "./routes/home/home-page/home.component.tsx";
 import NavigationComponent from "./routes/navigation/navigation.component.tsx";
-
 import LoginPageComponent from "./routes/authentification/login-page/login-page.component.tsx";
-import ContactsComponent from "./routes/contacts/contacts.component.tsx";
+import ContactsComponent from "./routes/contacts/contacts-page/contacts.component.tsx";
 import ShopComponent from "./routes/shop/shop.component.tsx";
+import CartComponent from "./routes/cart/cart-page/cart.component.tsx";
 
-export type category = {
-	title: string;
-	id: number;
-	imageUrl: string;
-};
+import { CategoriesContext } from "./contexts/products.context.tsx";
+
 function App() {
+	const { selectedCategory } = useContext(CategoriesContext);
 	return (
 		<Routes>
 			<Route path="/" element={<NavigationComponent />}>
-				<Route index element={<HomeComponent />} />
-				<Route path={"contacts"} element={<ContactsComponent />} />
-				<Route index path="shop" element={<ShopComponent />} />
-				<Route path="sign-in" element={<LoginPageComponent />} />
+				<Route path="/contacts" element={<ContactsComponent />} />
+				<Route path="/shop" element={<HomeComponent />} />
+				<Route
+					path={`/shop/:${selectedCategory}`}
+					element={<ShopComponent />}
+				/>
+				<Route path="/sign-in" element={<LoginPageComponent />} />
+				<Route path="/shopping-cart" element={<CartComponent />} />
 			</Route>
 		</Routes>
 	);
