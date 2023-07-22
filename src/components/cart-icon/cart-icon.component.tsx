950484853214bb88923b6c373bba76810a02505d
Valeriu <cazacuvaleriu2@gmail.com>
2023-07-22 09:56:20 +0300

    Implemented the feature to show the count of items in cart at the cart icon

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,12 +1,16 @@
-import { ReactComponent as ShoppingCart } from "../../assets/shopping-cart.svg";
+import { useContext } from "react";
 
+import { CartContext } from "../../contexts/cart.context.tsx";
+
+import shoppingCart from "../../assets/shopping-cart.png";
 import "./cart-icon.styles.scss";
 
 const CartIconComponent = () => {
+	const { cartProducts } = useContext(CartContext);
 	return (
 		<div className="cart-icon">
-			<ShoppingCart />
-			<span className="cart-icon__count">0</span>
+			<img src={shoppingCart} alt={shoppingCart} width={50} height={50} />
+			<span className="cart-icon__count">{cartProducts.length}</span>
 		</div>
 	);
 };
