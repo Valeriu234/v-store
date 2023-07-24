d5925a927273c1a79f419b8d1d663a7ca072119a
Valeriu <cazacuvaleriu2@gmail.com>
2023-07-24 11:38:58 +0300

    Show products quantity in cart icon

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -6,11 +6,11 @@ import shoppingCart from "../../assets/shopping-cart.png";
 import "./cart-icon.styles.scss";
 
 const CartIconComponent = () => {
-	const { cartProducts } = useContext(CartContext);
+	const { productsQuantity } = useContext(CartContext);
 	return (
 		<div className="cart-icon">
 			<img src={shoppingCart} alt={shoppingCart} width={50} height={50} />
-			<span className="cart-icon__count">{cartProducts.length}</span>
+			<span className="cart-icon__count">{productsQuantity}</span>
 		</div>
 	);
 };
