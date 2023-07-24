49454020472ec5abdc4df3b4e9b813462ad0b86c
Valeriu <cazacuvaleriu2@gmail.com>
2023-07-24 12:01:10 +0300

    If user is not signed in when he tries to add to cart he will get the no user advertisment

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,9 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { CartContext, Product } from "../../contexts/cart.context.tsx";
 import ButtonComponent from "../button/button.component.tsx";
 
 import "./product-card.styles.scss";
+import { UserContext } from "../../contexts/user.context.tsx";
+import PopupComponent from "../popup/popup.component.tsx";
+import NoUserAdvertismentComponent from "../no-user-advertisment/no-user-advertisment.component.tsx";
 
 interface ProductCardProps {
 	product: Product;
@@ -11,22 +14,35 @@ interface ProductCardProps {
 const ProductCardComponent = ({ product }: ProductCardProps) => {
 	const { name, imageUrl, price } = product;
 	const { addToCart } = useContext(CartContext);
+	const { currentUser } = useContext(UserContext);
+	const [isAdvertisingPopup, setIsAdvertisingPopup] = useState(false);
 
 	const addToCartHandler = () => {
-		addToCart(product);
+		if (currentUser) addToCart(product);
+		else setIsAdvertisingPopup(true);
 	};
-
+	const closeAdvertisingPopup = () => {
+		setIsAdvertisingPopup(false);
+	};
 	return (
-		<div className="product-card">
-			<img src={imageUrl} alt={`${name}`} />
-			<div className="product-card__information">
-				<span className="information__name">{name}</span>
-				<span className="information__price">{price}</span>
+		<>
+			<div className="product-card">
+				<img src={imageUrl} alt={`${name}`} />
+				<div className="product-card__information">
+					<span className="information__name">{name}</span>
+					<span className="information__price">{price}$</span>
+				</div>
+				<ButtonComponent onClick={addToCartHandler} buttonType="inverted">
+					Add to cart
+				</ButtonComponent>
 			</div>
-			<ButtonComponent onClick={addToCartHandler} buttonType="inverted">
-				Add to cart
-			</ButtonComponent>
-		</div>
+			<PopupComponent
+				isOpen={isAdvertisingPopup}
+				onClose={closeAdvertisingPopup}
+			>
+				<NoUserAdvertismentComponent />
+			</PopupComponent>
+		</>
 	);
 };
 
